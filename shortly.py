ceb349be187d91273ec16debd8c9bf3d11468955
sti320a <sti320a@gmail.com>
2018-12-09 01:22:15 +0900

    Complete turotial

--- shortly.py
+++ shortly.py
@@ -12,15 +12,70 @@ from jinja2 import Environment, FileSystemLoader
 class Shortly(object):
 
     def __init__(self, config):
+        # redisの設定
         self.redis = redis.Redis(config['redis_host'], config['redis_port'])
+        # templatesのパスを取得
+        template_path = os.path.join(os.path.dirname(__file__), 'templates')
+        # Jinja2にtemplatesのパスを教えてあげる
+        self.jinja_env = Environment(lodaer=FileSystemLoader(template_path), autoescape=True)
+
+        # ルーティング
+        self.url_map = Map([
+            Rule('/', endpoint='new_url'),
+            Rule('/<short_id>', endpoint='follow_short_link'),
+            Rule('/<short_id>+', endpoint='short_link_details')
+        ])
+
+    # テンプレートをレンダリングするメソッド
+    def render_template(self, template_nama, **context):
+        # __init__で指定したディレクトリから、template_nameにあたるファイルのパスを取得
+        t = self.jinja_env.get_template(template_name) 
+        # レンダリングして返す
+        return Response(t.render(context), mimetype='text/html')
 
     def dispatch_request(self, request):
+        adapter = self.url_map.bind_to_environ(request.environ)
+        try:
+            endpoint, values = adapter.match()
+            return getattr(self, 'on_' + endpoint)(request, **values)
+        except HTTPException, e:
+            return e
+
         return Response('Hello World!')
 
     def wsgi_app(self, environ, start_response):
         request = Request(environ)
         response = self.dispatch_request(request)
         return response(environ, start_response)
+
+    def on_new_url(self, request):
+        error = None
+        url = ''
+        if request.method == 'POST':
+            url = request.form['url']
+            if not is_valid_url(url):
+                error = 'Please enter a valid URL'
+            else:
+                short_id = self.insert_url(url)
+                return redirect('%s+'%short_id)
+            return self.render_template('new_url.html', error=error, url=url)
+
+    def insert_url(self, url):
+        short_id = self.redis.get('reverse-url:' + url)
+        if short_id is not None:
+            return short_id
+        url_num = self.redis.incr('last-url-id')
+        short_id = base36_encode(url_num)
+        self.redis.set('url-target:' + short_id, url)
+        self.redis.set('reverse-url:' + url, short_id)
+        return short_id
+
+    def on_follow_short_link(self, request, short_id):
+        link_target = self.redis.get('url-target:' + short_id)
+        if link_target is None:
+            raise NotFound()
+        self.redis.incr('click-count:' + short_id)
+        return redirect(link_target)
 
     def __call__(self, environ, start_response):
         return self.wsgi_app(environ, start_response)
@@ -37,6 +92,21 @@ def create_app(redis_host='localhost', redis_port=6379, with_static=True):
         })
     return app
 
+
+def is_valid_url(url):
+    parts = urlparse.urlparse(url)
+    return parts.scheme in ('http', 'https')
+
+
+def base36_encode(number):
+    assert number >= 0, 'positive integer required'
+    if number == 0:
+        return '0'
+    base36 = []
+    while number != 0:
+        number, i = divmod(number, 36)
+        base36.append('0123456789abcdefghijklmnopqrstuvwxyz'[i])
+    return ''.join(reversed(base36))    
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
